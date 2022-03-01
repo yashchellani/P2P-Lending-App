dee0a2ab1802e6f86a3f483bc9db7316b7259169
yunsoe <yunsoe3105@gmail.com>
2022-03-01 18:12:19 +0800

    set up

---
+++ g1t2/src/main/java/com/example/g1t2/G1t2Application.java
@@ -0,0 +1,13 @@
+package com.example.g1t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1t2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1t2Application.class, args);
+	}
+
+}
---
+++ g1t2/src/test/java/com/example/g1t2/G1t2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g1t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1t2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
