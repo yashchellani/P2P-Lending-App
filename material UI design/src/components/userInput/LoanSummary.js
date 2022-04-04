873e836a0f105cdfa82f7b43920c0feb408ad9a3
yunsoe <yunsoe3105@gmail.com>
2022-04-04 13:41:54 +0800

    Update LoanSummary.js

--- material UI design/src/components/userInput/LoanSummary.js
+++ material UI design/src/components/userInput/LoanSummary.js
@@ -5,7 +5,7 @@ export default function LoanSummary(props) {
             <h1>Loan Summary</h1>
             <h3>Loan Amount: ${props.data.amount} </h3>
             <h3>Interest rate: {props.data.interestRate}% </h3>
-            <h3>Monthly payback date: {props.data.date}st of every month </h3>
+            <h3>Monthly payback date: {props.data.date} of every month </h3>
             <h3>Payback period: {props.data.period} month(s) </h3>
         </div>
 
