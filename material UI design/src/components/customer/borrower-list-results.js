995fde79ea209e9d01f5500ba8045a6e9ada7e9b
ELAbishek <elabishek.2020@smu.edu.sg>
2022-04-04 13:29:42 +0800

    Removed styled component

--- material UI design/src/components/customer/borrower-list-results.js
+++ material UI design/src/components/customer/borrower-list-results.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
-import styled from "styled-components";
 import {
   Avatar,
   Box,
@@ -30,25 +29,6 @@ const theme = {
     hover: "#ad1457"
   }
 };
-const Button = styled.button`
-  background-color: ${(props) => theme[props.theme].default};
-  color: white;
-  padding: 5px 15px;
-  border-radius: 5px;
-  outline: 0;
-  text-transform: uppercase;
-  margin: 10px 0px;
-  cursor: pointer;
-  box-shadow: 0px 2px 2px lightgray;
-  transition: ease background-color 250ms;
-  &:hover {
-    background-color: ${(props) => theme[props.theme].hover};
-  }
-  &:disabled {
-    cursor: default;
-    opacity: 0.7;
-  }
-`;
 export const CustomerListResults = ({ customers, ...rest }) => {
   const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
   const [limit, setLimit] = useState(10);
